c7d0ac57b511f075f3492c3da5f6c1028d643476
iampersistent <corvus@zestic.com>
2025-03-26 13:56:00 -0500

    fix: remove unnecessary RuntimeException use statements from migrations

--- resources/db/migrations/postgres/20241209025249_create_email_token_table.php
+++ resources/db/migrations/postgres/20241209025249_create_email_token_table.php
@@ -1,7 +1,6 @@
 <?php
 
 use Phinx\Migration\AbstractMigration;
-use RuntimeException;
 
 class CreateEmailTokenTable extends AbstractMigration
 {
--- resources/db/migrations/postgres/20241209170738_create_users_table.php
+++ resources/db/migrations/postgres/20241209170738_create_users_table.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 use Phinx\Migration\AbstractMigration;
-use RuntimeException;
 
 final class CreateUsersTable extends AbstractMigration
 {

5554b49cd504f49493b555efe2587783ef94c315
iampersistent <corvus@zestic.com>
2025-07-30 08:59:38 -0500

    CS

--- tests/Integration/DB/PDO/AuthCodeRepositoryTest.php
+++ tests/Integration/DB/PDO/AuthCodeRepositoryTest.php
@@ -25,11 +25,14 @@ class AuthCodeRepositoryTest extends DatabaseTestCase
         // For MySQL, this will work as a string
         return sprintf(
             '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-            mt_rand(0, 0xffff), mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff),
             mt_rand(0, 0xffff),
             mt_rand(0, 0x0fff) | 0x4000,
             mt_rand(0, 0x3fff) | 0x8000,
-            mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff)
         );
     }
 
